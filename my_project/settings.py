7604ffd91bde02086d41e45abd27522eb56256ca
undon171 <dgkwells@outlook.com>
2026-02-03 20:46:20 +0000

    Replace hello_world app with home app, add Post model and migrations

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world',
     'about',
+    'home',
 ]
 
 MIDDLEWARE = [
