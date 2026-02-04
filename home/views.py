ebcb4daba9752dcb8531b44b9da25895922d7ecf
undon171 <dgkwells@outlook.com>
2026-02-04 13:06:29 +0000

    adjusted index.html location

--- home/views.py
+++ home/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("Hello, world! This is my first Django view.")
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse("Restaurant")
+    
+        return render(request, "/templates/index.html")
