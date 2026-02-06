a2937885bdb022f159fd03443064264967a31cda
undon171 <dgkwells@outlook.com>
2026-02-06 11:27:19 +0000

    Add email configuration for booking confirmations

--- my_project/settings.py
+++ my_project/settings.py
@@ -142,3 +142,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Django Restaurant <noreply@djangorestaurant.com>')
+
+# Authentication settings
+LOGIN_REDIRECT_URL = 'my_bookings'
+LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
